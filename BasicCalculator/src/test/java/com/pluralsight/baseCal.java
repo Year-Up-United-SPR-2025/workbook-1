4d4ef5cb012fbe3cd40ae99db40ae4e6468aed02
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-14 16:04:08 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

---
+++ BasicCalculator/src/test/java/com/pluralsight/baseCal.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class baseCal {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the first number?");
+        int number1 = scanner.nextInt();
+        System.out.println("What is the second number?");
+        int number2 = scanner.nextInt();
+
+        System.out.println("Possible calculations:");
+        System.out.println("(A)dd");
+        System.out.println("(S)ubtract");
+        System.out.println("(M)ultiply");
+        System.out.println("(D)ivide");
+
+        System.out.print("Please select an option: ");
+        char option = scanner.next().charAt(0);
+
+        if (option == 'M' || option == 'm') {
+            int result = number1 * number2;
+            System.out.println(number1 + " * " + number2 + " = " + result);
+        }
+        scanner.close();
+
+    }
+
+}
