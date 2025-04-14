8da89bc04e0bfabc1748115bba0a072b774ccd52
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-14 16:36:47 -0400

    Completed Mod3 - Exercise 4 - Payroll Calculator

---
+++ PayrollCalculator/src/test/java/com/pluralsight/payCal.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class payCal {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Name Scan
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Hours Worked Scan
+        System.out.print("Enter hours worked: ");
+        double hoursWorked = scanner.nextDouble();
+
+        // Pay Rate Sacn
+        System.out.print("Enter pay rate: ");
+        double payRate = scanner.nextDouble();
+
+        // Calculate Gross Pay Scan
+        double grossPay = hoursWorked * payRate;
+
+        // Display results Scan
+        System.out.println("\nEmployee Name: " + name);
+        System.out.println("Gross Pay: $" + grossPay);
+
+        scanner.close();
+    }
+}
