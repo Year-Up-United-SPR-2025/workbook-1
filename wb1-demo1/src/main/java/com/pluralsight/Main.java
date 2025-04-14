639cd9c0085d766b1be796515aab5db6e383cc20
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-14 10:53:57 -0400

    1st example of expression of operators

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,25 @@
 package com.pluralsight;
 
-public class Main {
-    public static void main(String[] args) {
 
 
+public class Main { //this is the start of a class
 
-        System.out.println("Hello, World!");
+
+//a method
+    public static void main(String[] args ) { //start of argument
+
+    String word1 = "Hello"; //the text in the double quotes are a string 
+
+    String word2= "World"; //its being expressed as a literal
+
+    String greeting;
+
+    greeting = word1 + " " + word2 + "!"; //a concatenation
+
+        System.out.println(greeting);
     }
+
 }
+//end method
+
+//This is the end of the class
