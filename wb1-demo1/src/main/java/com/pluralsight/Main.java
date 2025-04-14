df0931e0888ac60847614cb67a76233ae717603f
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-14 11:52:15 -0400

    added some stuff

--- wb1-demo1/src/main/java/com/pluralsight/Main.java
+++ wb1-demo1/src/main/java/com/pluralsight/Main.java
@@ -8,7 +8,7 @@ public class Main { //this is the start of a class
 //a method
     public static void main(String[] args ) { //start of argument
 
-    String word1 = "Hello"; //the text in the double quotes are a string 
+    String word1 = "Hello"; //the text in the double quotes are a string
 
     String word2= "World"; //its being expressed as a literal
 
@@ -17,6 +17,36 @@ public class Main { //this is the start of a class
     greeting = word1 + " " + word2 + "!"; //a concatenation
 
         System.out.println(greeting);
+
+
+        ////////////////////////////////////////////////////
+
+        float taxRate = 0.15f;
+
+        long aLargeNum = 222222L;
+
+
+        ////////////////////////////////////////////////
+
+        int aint;
+
+        long blong;
+
+        aint = 7;
+
+        blong = aint;
+
+
+        /// ///////////////////////////
+
+        long aLong;
+        int bInt;
+
+        aLong =7_000_000_000L; //makes an overflow -1589934592
+        //bInt = aLong; //gives error possible lossy conversion from long to int if not using type casting
+        bInt = (int) aLong; //using type casting by the (int) =
+
+        System.out.println(bInt);
     }
 
 }
