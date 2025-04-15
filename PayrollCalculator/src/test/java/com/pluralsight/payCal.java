1667e41e79fd76834147cb657e53dda90d4efa53
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-15 15:08:59 -0400

    added code the professor did

--- PayrollCalculator/src/test/java/com/pluralsight/payCal.java
+++ PayrollCalculator/src/test/java/com/pluralsight/payCal.java
@@ -3,42 +3,63 @@ import java.util.Scanner;
 
 public class payCal {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        // Name Scan
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        // Hours Worked Scan
-        System.out.print("Enter hours worked: ");
-        float hoursWorked = scanner.nextFloat();
-
-        // Pay Rate Scan
-        System.out.print("Enter pay rate: ");
-        double payRate = scanner.nextDouble();
+        //Get the known values.
+        String name = promptForName();
+        float hoursWorked = promptForHoursWorked();
+        double payRate = promptForPayRate();
 
 
-        double grossPay;  //=hoursWorked * payRate;
+        //Calculate the unknown
+        double grossPay = calcGrossPay(hoursWorked, payRate);
 
-        // Calculate Gross Pay Scan
-        if (hoursWorked <= 40){
-            grossPay = hoursWorked * payRate;
-        }
-        else {
-            float overTimeHours = hoursWorked - 40;
-            double basePay = 40 * payRate;
-            double overtimeRate = payRate * 1.5;
-            double overtimePay = overTimeHours * overtimeRate;
+        //Display the results
+        System.out.printf("%s worked %f hours at a rate of %f and made %f", name, hoursWorked, payRate, grossPay);
 
-            grossPay = basePay + overtimePay;
-        }
-
-        // Display results Scan
-        System.out.println("\nEmployee Name: " + name);
-        System.out.println("Gross Pay: $" + grossPay);
-
-        scanner.close();
     }
+
+    /**
+     * Calculates Gross Pay, considers overtime over 40 hours.
+     * @param hoursWorked Number of hours worked in the pay week
+     * @param payRate Amount paid per hour (base rate)
+     * @return amount earned during pay period.
+     */
+    public static double calcGrossPay(float hoursWorked, double payRate){
+//        if (hoursWorked <= 40){
+//            return hoursWorked * payRate;
+//        }
+//        else{
+//
+//            float overtimeHours = hoursWorked - 40;
+//            double basePay = 40 * payRate;
+//            double overtimeRate = payRate * 1.5;
+//            double overtimePay = overtimeHours * overtimeRate;
+//
+//            return basePay + overtimePay;
+//        }
+
+        return (hoursWorked > 40) ?  (40 * payRate) + ((hoursWorked - 40) * payRate * 1.5) :  hoursWorked * payRate;
+
+    }
+
+    public static String promptForName(){
+        System.out.print("Please enter the employee name: ");
+        return scanner.nextLine();
+
+    }
+
+    public  static float promptForHoursWorked(){
+        System.out.print("Please enter the number of hours worked this pay period: ");
+        return  scanner.nextFloat();
+    }
+
+    public static double promptForPayRate(){
+
+        System.out.print("Please enter the base pay rate: ");
+        return scanner.nextDouble();
+    }
+
 }
